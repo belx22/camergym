abaf9f9cb4eee77d5764a239dc59eb36b361e9eb
Bello mamoudou <bellofidele@gmail.com>
2025-02-27 13:02:25 +0100

    Update sync-to-github.php

--- sync-to-github.php
+++ sync-to-github.php
@@ -1,7 +1,8 @@
 <?php
-
+echo "debut operation";
 $projectPath = __DIR__; // Changez si besoin
 
+echo $projectPath;
 // Vérification de l'état du dépôt Git
 exec("cd $projectPath && git status --porcelain", $output);
 
