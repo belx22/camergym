2ca3d2b6a15254872c956634d5eade4cc0a4d2fa
Bello mamoudou <bellofidele@gmail.com>
2025-02-27 11:39:27 +0100

    Update formulaire.php

--- formulaire.php
+++ formulaire.php
@@ -1,3 +1,16 @@
+<?php
+if ($_GET['token'] !== 'votre_token_secret') {
+    die('Accès interdit');
+}
+
+// Exemple : vider le cache
+exec('php artisan cache:clear');
+
+// Exemple : exécuter une migration
+exec('php artisan migrate --force');
+
+echo "Déploiement terminé";
+?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
