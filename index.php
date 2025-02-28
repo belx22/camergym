0406377a09f147f8304938af54eea5c452ef79a0
Bello mamoudou <bellofidele@gmail.com>
2025-02-28 18:22:35 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription Gymnastique</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container text-center mt-5">
+        <img src="logo.png" alt="Logo" class="logo">
+        <h1 class="mt-3">Bienvenue sur la plateforme d'inscription</h1>
+        <div class="mt-4">
+            <a href="form_gymnaste.php" class="btn btn-primary btn-lg">Inscription Gymnaste</a>
+            <a href="form_juge.php" class="btn btn-secondary btn-lg">Inscription Juge</a>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="script.js"></script>
+</body>
+</html>
